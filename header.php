32a7ef0302bcb458d2bf65fe60d8b90d4cb77e78
Mikhail Panichev <denis.invader@gmail.com>
2017-05-05 12:03:06 +0700

    Initial commit

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>All japan</title>
+    </head>
+    <body>
+        <h1>All japan</h1>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php require_once './header.php';?>
+<div class="">
+    content
+</div>
+<?php require_once './footer.php';?>
