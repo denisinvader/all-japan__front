691b388aa2f2e7244aa33f6bc58d320f2a5eb014
Mikhail Panichev <denis.invader@gmail.com>
2017-05-21 17:42:19 +0700

    Pagination

---
+++ pagination.php
@@ -0,0 +1,11 @@
+<nav class="pagination">
+    <a href="#">1</a>
+    <span class="more">...</span>
+    <a href="#">6</a>
+    <a href="#">7</a>
+    <span class="active">8</span>
+    <a href="#">9</a>
+    <a href="#">10</a>
+    <span class="more">...</span>
+    <a href="#">17</a>
+</nav>
