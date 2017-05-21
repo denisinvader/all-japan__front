a53bbde200479b607970f82ffd039ab3296d975a
Mikhail Panichev <denis.invader@gmail.com>
2017-05-21 17:48:35 +0700

    News page

---
+++ news.php
@@ -0,0 +1,174 @@
+<?php require('./header.php');?>
+<main class="container news-list">
+    <div class="breadcrumbs">
+        <a href="/">Главная</a>
+        >
+        <span>Новости</span>
+    </div>
+
+    <h1>Новости компании</h1>
+
+    <div class="news-preview news-list__item">
+        <h2 class="news-preview__title">
+            <a href="#">Новое поступление контрактных двигателей</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="http://awtozapchasti.com/wp-content/uploads/2015/07/kntraktnye_dvigateli1.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                28 мая в автотехсервис Вся Япония прибыли контрактные двигатели для японских автомобилей разных марок.
+            </p>
+            <div class="news-preview__date">
+                28.05.2017
+            </div>
+        </div>
+    </div>
+    <div class="news-preview news-list__item">
+        <h2 class="news-preview__title">
+            <a href="#">Весь апрель развал-схождение в подарок</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="http://www.bochka-service.ru/admin/material_tmp/1436298236_0.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                Весь апрель 2017 года пройдите ТО в нашем сервисном центре и мы бесплатно сделаем вам развал схождение, если ваш автомобиль произведён в Японии. Читайте подробнее.
+            </p>
+            <div class="news-preview__date">
+                30.03.2017
+            </div>
+        </div>
+    </div>
+    <div class="news-preview news-list__item">
+        <h2 class="news-preview__title">
+            <a href="#">Услуги технического обслуживания</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="http://rusauto53.ru/tinybrowser/images/_full/_to-auto.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                Равным образом сложившаяся структура организации влечет за собой процесс внедрения и модернизации модели развития. Разнообразный и богатый опыт консультация с широким активом влечет за собой процесс внедрения и модернизации дальнейших направлений развития. Равным образом реализация намеченных плановых заданий в значительной степени обуславливает создание дальнейших направлений развития.
+            </p>
+            <div class="news-preview__date">
+                28.04.2017
+            </div>
+        </div>
+    </div>
+    <div class="news-preview news-list__item">
+        <h2 class="news-preview__title">
+            <a href="#">Как выбрать масло для автомобиля</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="http://img.autodni.ru/binaries/v3_bigimage/283894.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                Идейные соображения высшего порядка, а также реализация намеченных плановых заданий требуют определения и уточнения существенных финансовых и административных условий. Повседневная практика показывает, что дальнейшее развитие различных форм деятельности играет важную роль в формировании дальнейших направлений развития.
+            </p>
+            <div class="news-preview__date">
+                28.05.2017
+            </div>
+        </div>
+    </div>
+    <div class="news-preview news-list__item">
+        <h2 class="news-preview__title">
+            <a href="#">Большой ассортимент японских грузовиков</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="http://static.ngs.ru/news/preview/97b639b7bb7eb6583a439edd5d64ac3208e827fb_1000.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                Задача организации, в особенности же дальнейшее развитие различных форм деятельности требуют от нас анализа новых предложений. Разнообразный и богатый опыт сложившаяся структура организации требуют от нас анализа модели развития. Не следует, однако забывать, что новая модель организационной деятельности в значительной степени обуславливает создание системы обучения кадров.
+            </p>
+            <div class="news-preview__date">
+                15.03.2017
+            </div>
+        </div>
+    </div>
+    <div class="news-preview news-list__item">
+        <h2 class="news-preview__title">
+            <a href="#">Новое поступление спецтехники</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="http://domkrat.org/wp-content/uploads/2015/12/Spetstehnika-----vyigodnaya-arenda.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                Идейные соображения высшего порядка, а также новая модель организационной деятельности влечет за собой процесс внедрения и модернизации направлений прогрессивного развития. Задача организации, в особенности же реализация намеченных плановых заданий обеспечивает широкому кругу (специалистов) участие в формировании систем массового участия.
+            </p>
+            <div class="news-preview__date">
+                14.04.2017
+            </div>
+        </div>
+    </div>
+    <div class="news-preview news-list__item">
+        <h2 class="news-preview__title">
+            <a href="#">Как выбрать тормозные колодки</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="https://www.autoshcool.ru/uploads/posts/2010-12/1291647397_skripyat_perednie_tormoznye_kolodki.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                Повседневная практика показывает, что реализация намеченных плановых заданий представляет собой интересный эксперимент проверки модели развития. Значимость этих проблем настолько очевидна, что укрепление и развитие структуры позволяет оценить значение новых предложений. Идейные соображения высшего порядка, а также укрепление и развитие структуры требуют от нас анализа направлений прогрессивного развития. Повседневная практика показывает, что консультация с широким активом позволяет выполнять важные задания по разработке новых предложений. Идейные соображения высшего порядка, а также дальнейшее развитие различных форм деятельности позволяет оценить значение дальнейших направлений развития. Не следует, однако забывать, что постоянный количественный рост и сфера нашей активности представляет собой интересный эксперимент проверки форм развития.
+            </p>
+            <div class="news-preview__date">
+                28.03.2017
+            </div>
+        </div>
+    </div>
+    <div class="news-preview news-list__item">
+        <h2 class="news-preview__title">
+            <a href="#">Что делать если течет радиатор</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="http://365cars.ru/wp-content/uploads/2015/11/techet-radiator-avtomobilya.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                Равным образом постоянный количественный рост и сфера нашей активности требуют от нас анализа системы обучения кадров, соответствует насущным потребностям. Разнообразный и богатый опыт начало повседневной работы по формированию позиции в значительной степени обуславливает создание систем массового участия. Разнообразный и богатый опыт начало повседневной работы по формированию позиции влечет за собой процесс внедрения и модернизации существенных финансовых и административных условий.
+            </p>
+            <div class="news-preview__date">
+                20.03.2017
+            </div>
+        </div>
+    </div>
+    <div class="news-preview news-list__item">
+        <h2 class="news-preview__title">
+            <a href="#">Новое поступление автозапчасте</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="http://идея-малого-бизнеса.рф/wp-content/uploads/2015/08/91fb52bc0f8a85af8b260c3d68a980ad.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                Значимость этих проблем настолько очевидна, что начало повседневной работы по формированию позиции обеспечивает широкому кругу (специалистов) участие в формировании модели развития.
+            </p>
+            <div class="news-preview__date">
+                02.03.2017
+            </div>
+        </div>
+    </div>
+    <div class="news-preview news-preview_last-item">
+        <h2 class="news-preview__title">
+            <a href="#">Диагностика подвески</a>
+        </h2>
+        <a href="#" class="news-preview__image">
+            <img src="http://zerga.ru/imj5/podvecka%20VAZ%201.jpg"/>
+        </a>
+        <div class="news-preview__content">
+            <p class="news-preview__text">
+                28 мая в автотехсервис Вся Япония прибыли контрактные двигатели для японских автомобилей разных марок.
+            </p>
+            <div class="news-preview__date">
+                20.02.2017
+            </div>
+        </div>
+    </div>
+
+    <?php require('./pagination.php');?>
+</main>
+<?php require('./footer.php');?>
