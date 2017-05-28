dc1a4444f993b3d779a9ba405069ba92f1ce5f90
Mikhail Panichev <denis.invader@gmail.com>
2017-05-28 23:05:14 +0700

    Open callback modal only on desktops

--- src/scripts/modals.js
+++ src/scripts/modals.js
@@ -1,10 +1,14 @@
-(function () {
-  $('a[href*="tel:"]').click(function (e) {
+function openCallbackOnDesktop (e) {
+  var width = $window.width();
+
+  if (width > bpTablet) {
     e.preventDefault();
 
     $.fancybox.open({
       src: '#callbackModal',
       type: 'inline'
     });
-});
-})();
+  }
+}
+
+$('a[href^="tel:"]').on('click', openCallbackOnDesktop);
